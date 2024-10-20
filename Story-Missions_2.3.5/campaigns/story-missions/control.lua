b54924b14c1da9a637166ebe919cf7a48b8568a5
GreenFlagCZE <l.mark@seznam.cz>
2024-10-20 16:12:12 +0200

    Add files via upload

---
+++ Story-Missions_2.3.5/campaigns/story-missions/control.lua
@@ -0,0 +1,18 @@
+-- require("level-01.control")
+-- require("level-02.control")
+-- require("level-03.control")
+-- require("level-04.control")
+-- require("level-05.control")
+-- require("level-06.control")
+-- require("level-07.control")
+-- require("level-08.control")
+-- require("level-09.control")
+-- require("level-10.control")
+
+
+-- local function on_configuration_changed()
+	-- game.reload_script()
+-- end
+
+-- script.on_init(on_configuration_changed)
+-- script.on_configuration_changed(on_configuration_changed)
---
+++ Story-Missions_2.3.5/data.lua
@@ -0,0 +1,14 @@
+
+
+
+
+data:extend(
+{
+  {
+    type = "sound",
+    name = "fall-ship",
+    filename = "__Story-Missions__/sounds/fall-ship.ogg",
+    volume = 0.8
+  }
+})
+
